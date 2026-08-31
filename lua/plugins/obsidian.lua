6cb472442d2d60a38127d5f2645039bb6669c661
Saiyed Shizain <saiyedshizain@gmail.com>
2026-09-01 02:23:22 +0530

    fix(obsidian): fix deprecation warnings and replace legacy commands

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -10,65 +10,62 @@ vim.pack.add { { src = gh 'obsidian-nvim/obsidian.nvim', version = vim.version.r
 require('obsidian').setup {
   workspaces = {
     {
-      name = 'personal',
-      path = '~/vaults/personal',
+      name = 'Lessons',
+      path = '~/Documents/Obsidian/Lessons',
     },
-  },
-
-  daily_notes = {
-    folder = 'dailies',
-    date_format = '%Y-%m-%d',
-  },
-
-  completion = {
-    blink = true,
-    nvim_cmp = false,
-    min_chars = 2,
   },
 
   picker = {
     name = 'telescope.nvim',
   },
 
-  -- Disable obsidian.nvim's built-in UI rendering.
+  notes_subdir = "6 - Main Notes",
+
+  templates = {
+    subdir = "5 - Templates",
+    date_format = "%d-%m-%Y %H:%M %p",
+    time_format = "%H:%M",
+  },
+
+  frontmatter = {
+    func = function(note)
+      return {}
+    end,
+  },
+
+  note_id_func = function(title)
+    if title ~= nil then
+      return title
+    else
+      return tostring(os.time())
+    end
+  end,
+
+  new_notes_location = "notes_subdir",
+
+ -- Disable obsidian.nvim's built-in UI rendering.
   -- render-markdown.nvim handles all visual decorations instead.
   ui = {
     enable = false,
   },
 
-  -- In-buffer mappings
-  mappings = {
-    -- Smart action: follows link, toggles checkbox, or searches tag
-    ['<cr>'] = {
-      action = function() return require('obsidian').util.smart_action() end,
-      opts = { buffer = true, expr = true },
-    },
-    -- Follow wiki/markdown links (falls back to native gf outside vault)
-    ['gf'] = {
-      action = function() return require('obsidian').util.gf_passthrough() end,
-      opts = { noremap = false, expr = true, buffer = true },
-    },
-  },
+  legacy_commands = false,
 }
 
 -- Obsidian keymaps
-vim.keymap.set('n', '<leader>os', '<cmd>ObsidianQuickSwitch<cr>', { desc = 'Obsidian: Quick switch note' })
-vim.keymap.set('n', '<leader>og', '<cmd>ObsidianSearch<cr>', { desc = 'Obsidian: Grep search vault' })
-vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<cr>', { desc = 'Obsidian: New note' })
-vim.keymap.set('n', '<leader>ot', '<cmd>ObsidianTags<cr>', { desc = 'Obsidian: Find tags' })
-vim.keymap.set('n', '<leader>ob', '<cmd>ObsidianBacklinks<cr>', { desc = 'Obsidian: Backlinks' })
-
--- Daily notes
-vim.keymap.set('n', '<leader>od', '<cmd>ObsidianToday<cr>', { desc = 'Obsidian: Today\'s note' })
-vim.keymap.set('n', '<leader>oD', '<cmd>ObsidianDailies<cr>', { desc = 'Obsidian: Daily notes picker' })
+vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian quick_switch<CR>", { desc = "Obsidian: Quick Switch" })
+vim.keymap.set('n', '<leader>og', '<cmd>Obsidian search<cr>', { desc = 'Obsidian: Grep search vault' })
+vim.keymap.set('n', '<leader>on', '<cmd>Obsidian new<cr>', { desc = 'Obsidian: New note' })
+vim.keymap.set("n", "<leader>om", "<cmd>Obsidian rename<CR>", { desc = "Obsidian: Rename/Move Note" })
+vim.keymap.set('n', '<leader>ot', '<cmd>Obsidian template<cr>', { desc = 'Obsidian: Apply Template' })
+vim.keymap.set('n', '<leader>ob', '<cmd>Obsidian backlinks<cr>', { desc = 'Obsidian: Backlinks' })
+vim.keymap.set("n", "<leader>of", "<cmd>Obsidian follow_link<CR>", { desc = "Obsidian: Follow Link" })
 
 -- Tools
-vim.keymap.set('n', '<leader>oo', '<cmd>ObsidianOpen<cr>', { desc = 'Obsidian: Open in desktop app' })
-vim.keymap.set('n', '<leader>or', '<cmd>ObsidianRename<cr>', { desc = 'Obsidian: Rename note & references' })
-vim.keymap.set('n', '<leader>oi', '<cmd>ObsidianPasteImg<cr>', { desc = 'Obsidian: Paste clipboard image' })
-vim.keymap.set('n', '<leader>op', '<cmd>ObsidianTemplate<cr>', { desc = 'Obsidian: Insert template' })
+vim.keymap.set('n', '<leader>or', '<cmd>Obsidian rename<cr>', { desc = 'Obsidian: Rename note & references' })
+vim.keymap.set('n', '<leader>oi', '<cmd>Obsidian paste_img<cr>', { desc = 'Obsidian: Paste clipboard image' })
 
 -- Visual mode
-vim.keymap.set('v', '<leader>ol', ':<C-u>ObsidianLink<cr>', { desc = 'Obsidian: Link selection to note' })
-vim.keymap.set('v', '<leader>on', ':<C-u>ObsidianLinkNew<cr>', { desc = 'Obsidian: Link selection to new note' })
-vim.keymap.set('v', '<leader>oe', ':<C-u>ObsidianExtractNote<cr>', { desc = 'Obsidian: Extract selection to new note' })
+vim.keymap.set('v', '<leader>ol', ':<C-u>Obsidian link<cr>', { desc = 'Obsidian: Link selection to note' })
+vim.keymap.set('v', '<leader>on', ':<C-u>Obsidian link_new<cr>', { desc = 'Obsidian: Link selection to new note' })
+vim.keymap.set('v', '<leader>oe', ':<C-u>Obsidian extract_note<cr>', { desc = 'Obsidian: Extract selection to new note' })
