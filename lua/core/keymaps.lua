481884a76579642addf86133801803f07b0fe136
Saiyed Shizain <saiyedshizain@gmail.com>
2026-09-01 00:12:37 +0530

    feat(keymap): add a vertical and horizontal split keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -49,6 +49,9 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+vim.keymap.set('n', '<leader>sv', '<cmd>vsplit<CR>', { desc = '[S]plit [V]ertical' })
+vim.keymap.set('n', '<leader>sh', '<cmd>split<CR>', { desc = '[S]plit [H]orizontal' })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
