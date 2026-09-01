c0147d98dec897dd931e241255beac7e51cffad5
Saiyed Shizain <saiyedshizain@gmail.com>
2026-09-01 22:56:03 +0530

    feat(editor): add rule to automatically close < > brackets

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -3,4 +3,10 @@
 
 local gh = require('core.utils').gh
 vim.pack.add { gh 'windwp/nvim-autopairs' }
-require('nvim-autopairs').setup {}
+local autopairs = require('nvim-autopairs')
+local Rule = require('nvim-autopairs.rule')
+
+autopairs.setup {}
+
+-- Add < > pair specifically as requested by the user
+autopairs.add_rule(Rule('<', '>'))
