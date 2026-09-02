6a99201a1043138146c10fb497a712225d96906a
Saiyed Shizain <saiyedshizain@gmail.com>
2026-09-02 22:07:40 +0530

    fix(ui): remove missing NvChad highlight groups from indent-blankline

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -9,6 +9,6 @@ local hooks = require "ibl.hooks"
 hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
 
 require('ibl').setup {
-  indent = { char = "│", highlight = "IblChar" },
-  scope = { char = "│", highlight = "IblScopeChar" },
+  indent = { char = "│" },
+  scope = { char = "│" },
 }
