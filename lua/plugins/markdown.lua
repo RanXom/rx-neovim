2216b0b3db9afd28a18f98b613f67b511014f4aa
Saiyed Shizain <saiyedshizain@gmail.com>
2026-09-03 11:38:49 +0530

    feat(markdown): keep rendering active in insert mode

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -8,6 +8,9 @@ local gh = require('core.utils').gh
 vim.pack.add { gh 'MeanderingProgrammer/render-markdown.nvim' }
 
 require('render-markdown').setup {
+  -- Render markdown in ALL modes (including Insert mode)
+  render_modes = true,
+  
   -- Disable built-in LaTeX unicode rendering.
   -- render-latex.nvim handles display math as images with higher fidelity.
   latex = {
