93f42a69cd19c485e8444f0ad8578b832de9d6ad
Saiyed Shizain <saiyedshizain@gmail.com>
2026-09-17 23:28:28 +0530

    feat(formatting): enable format on save with prettierd, stylua, black, clang-format and LSP fallback

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -10,29 +10,74 @@ vim.pack.add { gh 'stevearc/conform.nvim' }
 require('conform').setup {
   notify_on_error = false,
   format_on_save = function(bufnr)
-    -- You can specify filetypes to autoformat on save here:
-    local enabled_filetypes = {
-      -- lua = true,
-      -- python = true,
-    }
-    if enabled_filetypes[vim.bo[bufnr].filetype] then
-      return { timeout_ms = 500 }
-    else
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return nil
     end
+
+    -- Disable autoformat on save for markdown files by default (avoids messing up Obsidian notes/templates)
+    -- You can still manually format any buffer with <leader>f
+    local ignore_filetypes = { markdown = true }
+    if ignore_filetypes[vim.bo[bufnr].filetype] then
+      return nil
+    end
+
+    return {
+      timeout_ms = 1000,
+      lsp_format = 'fallback',
+    }
   end,
   default_format_opts = {
-    lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
+    lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting.
   },
-  -- You can also specify external formatters in here.
   formatters_by_ft = {
-    -- rust = { 'rustfmt' },
-    -- Conform can also run multiple formatters sequentially
-    -- python = { "isort", "black" },
-    --
-    -- You can use 'stop_after_first' to run the first available formatter from the list
-    -- javascript = { "prettierd", "prettier", stop_after_first = true },
+    lua = { 'stylua' },
+    python = { 'black' },
+    javascript = { 'prettierd', 'prettier', stop_after_first = true },
+    typescript = { 'prettierd', 'prettier', stop_after_first = true },
+    javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+    typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+    html = { 'prettierd', 'prettier', stop_after_first = true },
+    css = { 'prettierd', 'prettier', stop_after_first = true },
+    scss = { 'prettierd', 'prettier', stop_after_first = true },
+    json = { 'prettierd', 'prettier', stop_after_first = true },
+    jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+    yaml = { 'prettierd', 'prettier', stop_after_first = true },
+    c = { 'clang-format' },
+    cpp = { 'clang-format' },
   },
 }
 
-vim.keymap.set({ 'n', 'v' }, '<leader>f', function() require('conform').format { async = true } end, { desc = '[F]ormat buffer' })
+-- Keymap to manually format buffer
+vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
+  require('conform').format { async = true, lsp_format = 'fallback' }
+end, { desc = '[F]ormat buffer' })
+
+-- Keymap to toggle autoformat on save
+vim.keymap.set('n', '<leader>tf', function()
+  vim.b.disable_autoformat = not vim.b.disable_autoformat
+  local status = vim.b.disable_autoformat and 'disabled' or 'enabled'
+  vim.notify('Format-on-save ' .. status .. ' for this buffer', vim.log.levels.INFO)
+end, { desc = '[T]oggle [F]ormat on save (buffer)' })
+
+-- Commands to toggle autoformat on save
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    vim.g.disable_autoformat = true
+    vim.notify('Format-on-save disabled globally', vim.log.levels.WARN)
+  else
+    vim.b.disable_autoformat = true
+    vim.notify('Format-on-save disabled for current buffer', vim.log.levels.WARN)
+  end
+end, {
+  desc = 'Disable autoformat-on-save (use ! for global)',
+  bang = true,
+})
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+  vim.notify('Format-on-save enabled', vim.log.levels.INFO)
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
